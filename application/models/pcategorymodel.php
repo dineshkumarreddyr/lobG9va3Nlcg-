1d82adcf14b3a894d79b931552d966c6aa50942e
uday <uday.swamy@popcornapps.com>
2015-09-20 01:27:16 +0530

    cganges

--- application/models/pcategorymodel.php
+++ application/models/pcategorymodel.php
@@ -15,7 +15,7 @@ class Pcategorymodel extends CI_Model {
         $query = $this->db->query("SELECT * FROM p_categories WHERE pc_pid = ".$pid);
         }
         else {
-        $query = $this->db->query("SELECT DISTINCT(pc.pc_id) as pc_id, pc.* FROM p_categories pc, products p WHERE p.p_category = pc.pc_id AND p.p_gender = '".$gender."' AND  pc.pc_pid = ".$pid);
+        $query = $this->db->query("SELECT DISTINCT(pc.pc_id) as pc_id, pc.* FROM p_categories pc, products p WHERE p.p_category = pc.pc_id AND p.p_status = '1' AND p.p_gender = '".$gender."' AND  pc.pc_pid = ".$pid);
         }
         $data = $query->result();
         return $data;
