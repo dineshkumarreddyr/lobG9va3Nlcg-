0eb7112d2c75136a12b68f014d85078bd3581670
udaya kumar swamy vasa <udayakumarswamy@gmail.com>
2015-09-24 19:53:34 +0530

    updated admin edit category

--- application/modules/admin/views/pcategory/edit.php
+++ application/modules/admin/views/pcategory/edit.php
@@ -45,14 +45,14 @@
                               <option value="0">--Parent--</option>
                               <?php foreach ($cat_tree[0] as $key => $cat_tree1) {
                                 ?>
-                              <option value="<?php echo $cat_tree1; ?>"><?php echo $cat_list[$cat_tree1]; ?></option>
+                              <option value="<?php echo $cat_tree1; ?>" <?php if($cat_tree1 == $pcategory->pc_pid) { echo "selected"; } ?>><?php echo $cat_list[$cat_tree1]; ?></option>
                                 
                                 <?php if(array_key_exists($cat_tree1, $cat_tree)) { ?>
                                   <?php foreach ($cat_tree[$cat_tree1] as $key => $cat_tree2) { ?>
-                                  <option value="<?php echo $cat_tree2; ?>">-<?php echo $cat_list[$cat_tree2]; ?></option>
+                                  <option value="<?php echo $cat_tree2; ?>" <?php if($cat_tree2 == $pcategory->pc_pid) { echo "selected"; } ?>>-<?php echo $cat_list[$cat_tree2]; ?></option>
                                     <?php if(array_key_exists($cat_tree2, $cat_tree)) { ?>
                                       <?php foreach ($cat_tree[$cat_tree2] as $key => $cat_tree3) { ?>
-                                      <option value="<?php echo $cat_tree3; ?>">--<?php echo $cat_list[$cat_tree3]; ?></option>
+                                      <option value="<?php echo $cat_tree3; ?>" <?php if($cat_tree3 == $pcategory->pc_pid) { echo "selected"; } ?>>--<?php echo $cat_list[$cat_tree3]; ?></option>
                                       <?php } ?>
                                     <?php } ?>
                                   <?php } ?>
